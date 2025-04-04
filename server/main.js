7805fe29013215ec386d168b2b50f7418c09a2a2
Shishir D <shishird003@gmail.com>
2025-04-05 00:34:45 +0530

    Add About component and server setup with Express and MongoDB connection

---
+++ server/main.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv= require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());    
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const productRoutes=require('./routes/productRoutes');
+app.use('/api',productRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
